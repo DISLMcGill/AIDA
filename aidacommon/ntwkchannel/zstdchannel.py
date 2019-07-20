7084a8beffed30d76edd3910e39d1ca3a6512e2c
jdsilv2 <joseph.dsilva@mail.mcgill.ca>
2019-07-20 12:20:57 -0400

    Data transmission channel changes. modified:   aidacommon/ntwkchannel/zstdchannel.py new file:   aidacommon/ntwkchannel/zstdpchannel.py

--- aidacommon/ntwkchannel/zstdchannel.py
+++ aidacommon/ntwkchannel/zstdchannel.py
@@ -1,32 +1,21 @@
 from collections import OrderedDict
 import zstandard as zstd
-import time
-import logging
-from numpy import *
 import pickle
 import json_tricks as json
 
 def transmit(result, sock):
 
-
     #logging.debug("Ntwk channel json-zstd: to transmit : ");
 
-    numArray = len(result) #number of numpy arrays in the dict
-    pickler = pickle.Pickler(sock)
-    pickler.dump(numArray)
-    items = list(result.items())
-    
-    for i in range(numArray):
-        key = items[i][0]
-        pickler.dump(key)
-    
-    for i in range(numArray):
-        val = items[i][1]
-        y = json.dumps(val).encode('utf-8')
+    pickler = pickle.Pickler(sock);
+    cols = list(result.keys());
+    pickler.dump(cols);
+
+    for col in cols:
+        y = json.dumps(result[col]).encode('utf-8')
         cctx = zstd.ZstdCompressor()
         with cctx.stream_writer(sock) as compressor:
             compressor.write(y)
-
 
     #logging.debug("Ntwk channel json-zstd: transmission completed : ");
 
@@ -35,27 +24,18 @@ def receive(sock):
 
     #logging.debug("Ntwk channel json-zstd: waiting to receieve : ");
 
-    unpickler = pickle.Unpickler(sock)
+    unpickler = pickle.Unpickler(sock);
+    result = OrderedDict([]);
+    keylist = unpickler.load();
 
-    numArray = unpickler.load()
-    result = OrderedDict([])
-    keylist = []
-
-    for i in range(numArray):
-        key = unpickler.load()
-        keylist.append(key)
-
-    for i in range(numArray):
+    for col in keylist:
         data = ''
         dctx = zstd.ZstdDecompressor()
         
         for chunk in dctx.read_to_iter(sock, read_size=1):
             data += chunk.decode('utf-8')
 
-
-        val = json.loads(data)
-        result.update({key[i]:val})
-
+        result[col] = json.loads(data)
 
 
     #logging.debug("Ntwk channel json-zstd: data received : ");
