373ad61bff2dbe4e19dd8149dda45378c4b499d8
IEAISU <jianhao.cao@mail.mcgill.ca>
2020-09-09 11:02:56 -0400

    update python module

--- python_module/convert.c
+++ python_module/convert.c
@@ -21,6 +21,7 @@ static PyObject* convert_func(PyObject *self, PyObject *args){
     PyObject *keys;
     PyObject *dict;
     PyObject *arr;
+    PyObject *col_lists;
 
     // python decimal class
     PyObject * decimal_mod = PyImport_ImportModule("decimal");
@@ -44,39 +45,57 @@ static PyObject* convert_func(PyObject *self, PyObject *args){
     Py_INCREF(keys);
     dict = PyDict_New();
 
-    for (i = 0; i < cols; i++){
-        // get the key
-        PyObject *key = PyList_GetItem(keys, i);
-        PyObject *list = PyList_New( (Py_ssize_t)rows );
-        
-        // get elements
-        for (j = 0; j < rows; j++){
-            PyObject *row = PyList_GetItem(ob, j);
-            PyObject *data = PyDict_GetItem(row, key);
-            Py_INCREF(data);
-            PyList_SetItem(list, j, data);
-        }
 
-        // create a new numpy array to hold the data in a column
+
+
+
+    col_lists = PyDict_New();
+
+    for (i = 0; i < cols; i++){
+        PyObject *col_list = PyList_New( (Py_ssize_t)rows );
+        PyObject *key = PyList_GetItem(keys, i);
+        PyDict_SetItem(col_lists, key, col_list);
+    }
+
+    for (j = 0; j < rows; j++){
+        PyObject *row = PyList_GetItem(ob, j);
+        for (i = 0; i < cols; i++){
+            PyObject *key = PyList_GetItem(keys, i);
+	    Py_INCREF(key);
+            PyObject *col_list = PyDict_GetItem(col_lists, key);
+            PyObject *data = PyDict_GetItem(row, key);
+	    Py_DECREF(key);
+            Py_INCREF(data);
+            PyList_SetItem(col_list, j, data);
+        }
+    }
+
+    for (i = 0; i < cols; i++){
+        PyObject *key = PyList_GetItem(keys, i);
+        PyObject *col_list = PyDict_GetItem(col_lists, key);
+
         if (!rows){
-            arr = PyArray_FROM_OTF(list, NPY_NOTYPE, NPY_IN_ARRAY);
+            arr = PyArray_FROM_OTF(col_list, NPY_NOTYPE, NPY_IN_ARRAY);
         }else{
-            PyObject *first_element = PyList_GetItem(list, 0);
+            PyObject *first_element = PyList_GetItem(col_list, 0);
             if(PyLong_Check(first_element)){
-                arr = PyArray_FROM_OTF(list, NPY_INT64, NPY_IN_ARRAY);
+                arr = PyArray_FROM_OTF(col_list, NPY_INT64, NPY_IN_ARRAY);
             }else if(PyFloat_Check(first_element) || PyObject_IsInstance(first_element,decimal_cls)){
-                arr = PyArray_FROM_OTF(list, NPY_FLOAT64, NPY_IN_ARRAY);
+                arr = PyArray_FROM_OTF(col_list, NPY_FLOAT64, NPY_IN_ARRAY);
             }else{
-                arr = PyArray_FROM_OTF(list, NPY_OBJECT, NPY_IN_ARRAY);
+                arr = PyArray_FROM_OTF(col_list, NPY_OBJECT, NPY_IN_ARRAY);
             }
         }
 
-        Py_DECREF(list);
+        Py_DECREF(col_list);
         PyDict_SetItem(dict, key, arr);
     }
+    
+       
 
     Py_DECREF(ob);
     Py_DECREF(keys);
+    Py_DECREF(col_lists);
     Py_DECREF(decimal_mod);
     Py_DECREF(decimal_cls);
 
