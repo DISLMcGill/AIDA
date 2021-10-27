62503a6fbdf8f9584ddc505aa76637ccb6fae94b
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-10-26 21:29:20 -0400

    Added modules that is going to be used in the function as inputs for the operator

--- aidacommon/dbAdapter.py
+++ aidacommon/dbAdapter.py
@@ -437,12 +437,11 @@ class DBC(metaclass=ABCMeta):
         import numpy
         global sys
         import sys
-        print(sys.modules.keys())
         """Function that is called from stub to execute a python function in this workspace"""
         #Execute the function with this workspace as the argument and return the results if any.
         if(isinstance(func, str)):
             func = super().__getattribute__(func);
-        return func(self, *args, **kwargs);
+        return func(self, *args, **kwargs,nn = sys.modules["torch.nn.modules"],torch = sys.modules["torch"],datasets = sys.modules["sklearn.datasets"] );
 
     def _XP(self, func, *args, **kwargs):
         """Function that is called from stub to execute a python function in this workspace"""
