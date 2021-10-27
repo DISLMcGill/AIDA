da9cffb7e5e41c118114a9bbee3c4a7ddd45d49e
tonybca <70156811+tonybca@users.noreply.github.com>
2021-10-26 23:54:45 -0400

    first normalize trial

--- aidacommon/torchLinearRegressionTest3.py
+++ aidacommon/torchLinearRegressionTest3.py
@@ -1,6 +1,8 @@
 import copy
 
 from aida.aida import *;
+from aidacommon.dbAdapter import DataConversion
+
 host = 'Server3'; dbname = 'bixi'; user = 'bixi'; passwd = 'bixi'; jobName = 'torchLinear'; port = 55660;
 dw = AIDA.connect(host,dbname,user,passwd,jobName,port);
 def trainingLoop(dw,input_size, output_size,nn,torch,datasets):
@@ -12,7 +14,7 @@ def trainingLoop(dw,input_size, output_size,nn,torch,datasets):
     optimizer = torch.optim.SGD(model.parameters(), lr=learningrate)
     distance = dw.gmdata2017[:,2]
     duration = dw.gmdata2017[:,3]
-    X = DataConversion.extract_X(distance)
+    X = DataConversion.extract_y(distance)
     y = DataConversion.extract_y(duration)
     X = np.copy(X)
     y = np.copy(y)
