0c877eea3ea0a68b36775125d5aed738d54fc7b2
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-10-27 19:29:02 -0400

    imported torch.nn.functional at the server side and add that input to the client function

--- aidacommon/torchLinearRegressionTest3.py
+++ aidacommon/torchLinearRegressionTest3.py
@@ -7,7 +7,7 @@ host = 'tfServer2'; dbname = 'bixi'; user = 'bixi'; passwd = 'bixi'; jobName = '
 dw = AIDA.connect(host,dbname,user,passwd,jobName,port);
 def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F):
     learningrate = 0.01
-    epoch_size = 10000
+    epoch_size = 1000
     model = nn.Linear(input_size,output_size)
     model = model.cuda()
     criterion = nn.MSELoss()
@@ -30,6 +30,7 @@ def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
+        print(model.weight)
     dw.linearModel = model
     return(model.weight)
 
