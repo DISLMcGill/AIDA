42a0844a4aaa30189cb3ded40ea1f44eecf09885
tonybca <70156811+tonybca@users.noreply.github.com>
2021-10-26 23:41:46 -0400

    first normalize trial

--- aidacommon/torchLinearRegressionTest3.py
+++ aidacommon/torchLinearRegressionTest3.py
@@ -1,16 +1,23 @@
 from aida.aida import *;
 host = 'Server3'; dbname = 'bixi'; user = 'bixi'; passwd = 'bixi'; jobName = 'torchLinear'; port = 55660;
 dw = AIDA.connect(host,dbname,user,passwd,jobName,port);
-def trainingLoop(dw, input_size, output_size):
+def trainingLoop(dw,input_size, output_size,nn,torch,datasets):
     learningrate = 0.01
     epoch_size = 100
     model = nn.Linear(input_size,output_size)
     model = model.cuda()
     criterion = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learningrate)
-    X, y = datasets.make_regression(n_samples=100,n_features=1,noise=20,random_state=1)
+    distance = dw.gmdata2017[:,2]
+    duration = dw.gmdata2017[:,3]
+    X = DataConversion.extract_X(distance)
+    y = DataConversion.extract_y(duration)
+    X = X+0.01
+    y = y+0.01
     X = torch.from_numpy(X.astype(np.float32))
     y = torch.from_numpy(y.astype(np.float32))
+    X = nn.functional.normalize(X)
+    y = nn.functional.normalize(y)
     y = y.view(y.shape[0],1)
     X = X.cuda()
     y = y.cuda()
