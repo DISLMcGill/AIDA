32ff1e671660fa5b7da1d7bb9120cbd099bc316b
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-10-28 02:21:10 -0400

    Added a client training function for tensorflow

--- aidacommon/torchLinearRegressionTest3.py
+++ aidacommon/torchLinearRegressionTest3.py
@@ -1,11 +1,11 @@
 import copy
 
 from aida.aida import *;
-from aidacommon.dbAdapter import DataConversion
 host = 'Server2'; dbname = 'bixi'; user = 'bixi'; passwd = 'bixi'; jobName = 'torchLinear'; port = 55660;
 dw = AIDA.connect(host,dbname,user,passwd,jobName,port);
 def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F):
     import logging
+    from aidacommon.dbAdapter import DataConversion
     learningrate = 0.01
     epoch_size = 1000
     logging.info("running on server")
