2fc451376908ac8ce73eacab5f64a9abf2a880cc
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-11-01 02:38:44 -0400

    Added code for measuring the execution time

--- aidacommon/torchLinearRegressionTest3.py
+++ aidacommon/torchLinearRegressionTest3.py
@@ -4,10 +4,12 @@ from aida.aida import *;
 host = 'Server2'; dbname = 'bixi'; user = 'bixi'; passwd = 'bixi'; jobName = 'torchLinear'; port = 55660;
 dw = AIDA.connect(host,dbname,user,passwd,jobName,port);
 def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F,np):
+    import time
     import logging
     from aidacommon.dbAdapter import DataConversion
-    learningrate = 0.01
-    epoch_size = 1000
+    start_time = time.time()
+    learningrate = 0.0000001
+    epoch_size = 8000
     logging.info("running on server")
     model = nn.Linear(input_size,output_size)
     model = model.cuda()
@@ -22,7 +24,6 @@ def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F,np):
     X = torch.from_numpy(X.astype(np.float32))
     y = torch.from_numpy(y.astype(np.float32))
     y = y.view(y.shape[0],1)
-    X = F.normalize(X, dim=0)
     X = X.cuda()
     y = y.cuda()
     for epoch in range(epoch_size):
@@ -33,6 +34,9 @@ def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F,np):
         optimizer.zero_grad()
         print(model.weight)
     dw.linearModel = model
+    end_time = time.time()
+    execution_time = start_time - end_time
+    logging.info("execution time is",execution_time)
     return(model.weight)
 
 
