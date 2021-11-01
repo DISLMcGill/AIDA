1f2d39001458fbc80ccb1f4dd2cccc954db3c77c
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-11-01 03:52:20 -0400

    Added codes for performance analysis between CPU and GPU

--- aidacommon/torchLinearRegressionAnalysisGPU.py
+++ aidacommon/torchLinearRegressionAnalysisGPU.py
@@ -11,7 +11,7 @@ def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F,np):
     model = model.cuda()
     criterion = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learningrate)
-    X, y = datasets.make_regression(n_samples=2000000,n_features=1,noise=20,random_state=1)
+    X, y = datasets.make_regression(n_samples=1500000,n_features=1,noise=20,random_state=1)
     X = torch.from_numpy(X.astype(np.float32))
     y = torch.from_numpy(y.astype(np.float32))
     y = y.view(y.shape[0],1)
@@ -25,7 +25,7 @@ def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F,np):
         optimizer.zero_grad()
     end_time = time.time()
     execution_time = end_time - start_time
-    logging.info("execution time for 2000000 samples and 1000 iteration on GPU is " + str(execution_time))
+    logging.info("execution time for 1500000 samples and 1000 iteration on GPU is " + str(execution_time))
     return(model.weight)
 
 weight = dw._X(trainingLoop,1,1)
