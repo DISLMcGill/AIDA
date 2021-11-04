2b7183e25aacadf10e5626eb2e9202e0d10a77fe
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-11-04 02:19:35 -0400

    Added cpu usage analysis code

--- aidacommon/torchLinearRegressionAnalysisGPU.py
+++ aidacommon/torchLinearRegressionAnalysisGPU.py
@@ -11,7 +11,7 @@ def trainingLoop(dw,input_size, output_size,nn,torch,datasets,F,np):
     model = model.cuda()
     criterion = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learningrate)
-    X, y = datasets.make_regression(n_samples=130000,n_features=1,noise=20,random_state=1)
+    X, y = datasets.make_regression(n_samples=140000,n_features=1,noise=20,random_state=1)
     X = torch.from_numpy(X.astype(np.float32))
     y = torch.from_numpy(y.astype(np.float32))
     y = y.view(y.shape[0],1)
