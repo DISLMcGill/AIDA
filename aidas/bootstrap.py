ff5393b087442bbb1a79631a3eee9339d4463f0d
tonybca <haoteng.wang@mail.mcgill.ca>
2021-11-15 02:25:25 -0500

    Added keras

--- aidas/bootstrap.py
+++ aidas/bootstrap.py
@@ -102,7 +102,7 @@ def bootstrap():
     from tensorflow import keras
     builtins.keras = keras
     from tensorflow.keras import layers
-    builtins.keras = layers
+    builtins.layers = layers
 
 
 
