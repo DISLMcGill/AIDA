dd2e6f2abc6af6990bb47173a72fab1cf78544b2
tonybca <haoteng.wang@mail.mcgill.ca>
2021-11-17 10:04:40 -0500

    added customizable dataset for torch

--- aidacommon/AutoMPG-Torch.py
+++ aidacommon/AutoMPG-Torch.py
@@ -40,7 +40,7 @@ def trainingLoop(dw):
     df['C'] = randn(n)
     df['D'] = randn(n)
     df['E'] = randn(n)
-    df['Y'] = 5 + 3 * df.A + 6 * df.B ** 2 + 7 * df.C ** 3 + 2 * df.D ** 2 + 8 * df.E * df.D
+    df['Y'] = 5 + 3 * df.A + 6 * df.B ** 2 + 7 * df.C ** 3 + 2 * df.D ** 2 + 8 * df.E * df.D + randn(n)
 
     dataset = df.copy()
 
