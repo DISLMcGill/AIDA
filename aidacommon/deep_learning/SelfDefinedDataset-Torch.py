198fcc66a21e8b3610b9f3265ee12625dda6f787
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-11-23 03:38:08 -0500

    data size: 30000 epoch: 10000 16 hidden nodes  Torch

--- aidacommon/deep_learning/SelfDefinedDataset-Torch.py
+++ aidacommon/deep_learning/SelfDefinedDataset-Torch.py
@@ -125,8 +125,8 @@ def trainingLoop(dw):
         optimizer.zero_grad()
     end_time = time.time()
     execution_time = end_time - start_time
-    logging.info("The execution time on GPU for a dataset of size 30000 and 10 epochs using Pytorch is:", execution_time)
-    return_mesg = "The execution time on GPU for a dataset of size 10000 and 10 epochs using Pytorch is:" + str(execution_time)
+    logging.info("The execution time on GPU for a dataset of size 30000 and 10000 epochs using Pytorch is:", execution_time)
+    return_mesg = "The execution time on GPU for a dataset of size 30000 and 10000 epochs using Pytorch is:" + str(execution_time)
     # In[127]:
     normed_test_data = normed_test_data.cuda()
     test_target = test_target.cuda()
