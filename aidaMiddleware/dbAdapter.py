f2283d2657b11ca1b82a8ccb143db28da1d6e728
w-he <yu.j.he@mail.mcgill.ca>
2021-11-23 05:42:27 +0000

    some fixes

--- aidaMiddleware/dbAdapter.py
+++ aidaMiddleware/dbAdapter.py
@@ -5,6 +5,7 @@ import collections;
 import datetime;
 
 from aidacommon.dbAdapter import *;
+from aidaMonetDB import DBCMonetDB;
 from aidas.rdborm import *;
 from aidas.dborm import DBTable, DataFrame;
 from aida.aida import *;
@@ -39,9 +40,9 @@ class DBCMiddleware(DBC):
     def __new__(cls, dbname, username, password, jobName, dbcRepoMgr, serverIPAddr):
         """See if the connection works, authentication fails etc. In which case we do not need to continue with the object creation"""
         #logging.debug("__new__ called for {}".format(jobName));
-        con = AIDA.connection(dbname,hostname='whe_server_1',username=username,password=password,autocommit=True);
+        #con = AIDA.connect('whe_server_1', dbname, username, password, jobName, 55660);
         #logging.debug("connection test for {} result {}".format(jobName, con));
-        con.close();
+        #con.close();
         return super().__new__(cls);
 
     def __init__(self, dbname, username, password, jobName, dbcRepoMgr, serverIPAddr):
@@ -55,7 +56,7 @@ class DBCMiddleware(DBC):
         self.__setDBC__();
 
     def __setDBC__(self):
-        con = AIDA.connection(self.dbName,hostname='whe_server_1',username=self._username,password=self._password,port=55660);
+        con = AIDA.connect('whe_server_1', self._dbName,self._username,self._password,self._jobName,55660);
         ##cursor = con.cursor();
         #This function call should set the internal database connection to MonetDB in THIS DBC object, using the jobName passed to it.
         #The database function basically calls back the _setConnection method.
@@ -63,5 +64,21 @@ class DBCMiddleware(DBC):
         ##data = cursor.fetchall();
         #logging.debug("rows = {} data = {} after setting dbc con.".format(rows, data));
         ##cursor.close();
-        con.execute('select status from aidas_setdbccon(\'{}\');'.format(self._jobName));
+        #con.execute('select status from aidas_setdbccon(\'{}\');'.format(self._jobName));
         self.__extDBCcon = con;
+
+    def _getDBTable(self, relName, dbName=None):
+        #logging.debug(DBCMonetDB.__TABLE_METADATA_QRY__.format( dbName if(dbName) else self.dbName, relName));
+        (metaData_, rows) = self._executeQry(DBCMonetDB.__TABLE_METADATA_QRY__.format( dbName if(dbName is not None) else self._dbName, relName));
+        if(rows ==0):
+            logging.error("ERROR: cannot find table {} in {}".format(relName, dbName if(dbName is not None) else self._dbName ));
+            raise KeyError("ERROR: cannot find table {} in {}".format(relName, dbName if(dbName is not None) else self._dbName ));
+        #logging.debug("execute query returned for table metadata {}".format(metaData_));
+        #metaData = _collections.OrderedDict();
+        #for column in [ 'schemaname', 'tablename', 'columnname', 'columntype', 'columnsize', 'columnpos', 'columnnullable']:
+        #    logging.debug("For column {} data is {}".format(column, metaData_[column].data if hasattr(metaData_[column], 'data') else metaData_[column]));
+        #    metaData[column] = metaData_[column].data if hasattr(metaData_[column], 'data') else metaData_[column];
+
+        #return DBTable(self, metaData_);
+        d = DBTable(self, metaData_);
+        return d;
