18a69b88a1bf75413f10badf904133d9dee88d03
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-11-29 07:17:24 -0500

    Tried to transfer the training datset to gpu

--- aidacommon/tpch.py
+++ aidacommon/tpch.py
@@ -2,9 +2,9 @@ import logging
 import time
 import psycopg2
 logging.basicConfig(level=logging.INFO, filename='query.log')
-connection=psycopg2.connect(user='sf01',password='sf01',host='zchen94-postgre-server',database='sf01')
+connection=psycopg2.connect(user='bixi',password='bixi',host='tfNewServer',database='bixi')
 cursor=connection.cursor()
 while True:
     t1=time.time()
-    cursor.execute("SELECT SUM(l_extendedprice) / 7.0 AS avg_yearly FROM lineitem, part WHERE p_partkey = l_partkey AND p_brand = '[BRAND]' AND p_container = '[CONTAINER]' AND l_quantity < (SELECT 0.2 * AVG(l_quantity) FROM lineitem WHERE l_partkey = p_partkey);")
+    cursor.execute("SELECT * FROM gmdata2017;")
     logging.info("start:{}:elapsed:{}".format(t1,time.time()-t1))
