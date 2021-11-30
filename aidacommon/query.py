744b722cb3409d4c3ee4e05e161e78c32e4cef81
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-11-30 09:23:32 -0500

    Tried to transfer the training datset to gpu

--- aidacommon/query.py
+++ aidacommon/query.py
@@ -2,10 +2,13 @@ import logging
 import time
 import psycopg2
 logging.basicConfig(level=logging.INFO, filename='query.log')
+
 connection=psycopg2.connect(user='sf01',password='sf01',host='localhost',database='sf01')
 cursor=connection.cursor()
 while True:
     t1=time.time()
     cursor.execute("SELECT SUM(l_extendedprice) / 7.0 AS avg_yearly FROM lineitem, part WHERE p_partkey = l_partkey AND p_brand = '[BRAND]' AND p_container = '[CONTAINER]' AND l_quantity < (SELECT 0.2 * AVG(l_quantity) FROM lineitem WHERE l_partkey = p_partkey);")
-    logging.info("start:{}:elapsed:{}".format(t1,time.time()-t1))
-
+    length = time.time() - t1
+    logging.info("start:{}:elapsed:{}".format(t1,length))
+    with open('result.csv', 'a') as f:
+        f.write(str(t1)+','+str(length)+'\n')
