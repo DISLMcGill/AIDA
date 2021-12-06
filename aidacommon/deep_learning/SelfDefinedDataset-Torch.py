0068a4fb4b79479699482faaf033adb8d3a15cfc
Allenhuangrui <rui.huang4@mail.mcgill.ca>
2021-12-06 14:36:44 -0500

    10000 sample size 10 epochs

--- aidacommon/deep_learning/SelfDefinedDataset-Torch.py
+++ aidacommon/deep_learning/SelfDefinedDataset-Torch.py
@@ -4,7 +4,7 @@ dw = AIDA.connect(host,dbname,user,passwd,jobName,port);
 def trainingLoop(dw):
     script_start = time.time()
     logging.info('Script start time ' + str(script_start))
-    n = 1000
+    n = 10000
     df = pd.DataFrame(randn(n))
     df.columns = ['A']
     df['B'] = randn(n)
@@ -93,7 +93,7 @@ def trainingLoop(dw):
     # In[124]:
 
     criterion = nn.MSELoss()
-    epoch_size = 10000
+    epoch_size = 10
 
     # In[125]:
 
@@ -114,8 +114,8 @@ def trainingLoop(dw):
     end_time = time.time()
     logging.info('Training end time ' + str(end_time))
     execution_time = end_time - start_time
-    logging.info('The execution time on GPU for a dataset of size 1000 and 10000 epochs using Pytorch is: '+str(execution_time))
-    return_mesg = "The execution time on GPU for a dataset of size 1000 and 10000 epochs using Pytorch is:" + str(execution_time)
+    logging.info('The execution time on GPU for a dataset of size 10000 and 10 epochs using Pytorch is: '+str(execution_time))
+    return_mesg = "The execution time on GPU for a dataset of size 10000 and 10 epochs using Pytorch is:" + str(execution_time)
     # In[127]:
     normed_test_data = normed_test_data.cuda()
     test_target = test_target.cuda()
