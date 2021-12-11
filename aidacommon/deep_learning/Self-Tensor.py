be782be04606a0e509dd2bffe78c6341939b5c29
tonybca <haoteng.wang@mail.mcgill.ca>
2021-12-10 23:13:36 -0500

    changed host name

--- aidacommon/deep_learning/Self-Tensor.py
+++ aidacommon/deep_learning/Self-Tensor.py
@@ -12,7 +12,7 @@ def trainingLoop(dw):
     # tf.config.experimental.set_virtual_device_configuration(
     #     gpus[1],
     #     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=max_usage)])
-    n = 300
+    n = 10000
     df = pd.DataFrame(randn(n))
     df.columns = ['A']
     df['B'] = randn(n)
