61ccce04da211ef749422e11a2119199084a47bd
w-he <yu.j.he@mail.mcgill.ca>
2022-05-31 19:53:16 -0400

    fix db reading

--- aidaMiddleware/serverConfig.py
+++ aidaMiddleware/serverConfig.py
@@ -1,4 +1,5 @@
 import sqlite3
+import logging
 
 class ServerConfig:
    db_name = "/home/build/AIDA/aidaMiddleware/servers.db"
@@ -16,14 +17,19 @@ class ServerConfig:
          self.names = []
          for row in self.cursor.execute("SELECT hostname FROM servers"):
             self.names += [row[0]]
+
+         for row in self.cursor.execute("SELECT * FROM tables"):
+            if row[0] in self.table_partitions:
+               self.table_partitions[row[0]].append(row[1])
+            else:
+               self.table_partitions[row[0]] = [row[1]]
       return self.names
 
    def get_servers(self, table_name):
       if table_name in self.table_partitions:
          return self.table_partitions[table_name]
       else:
-         hosts = []
-         for row in self.cursor.execute(f"SELECT hosts FROM tables WHERE table = '{table_name}'"):
-            hosts += [row[0]]
-         self.table_partitions[table_name] = hosts
-      return self.table_partitions[table_name]
+         logging.error(
+            "ERROR: cannot find table {} in configuration".format(table_name);
+         raise KeyError(
+            "ERROR: cannot find table {} in configuration".format(table_name);
