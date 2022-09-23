d32a336623fcf8c0cbe1ed2ed6354f33ab5f6d02
winnie he <yu.j.he@mail.mcgill.ca>
2022-09-22 20:57:41 -0400

    fix pytorch

--- pytorch_mf.py
+++ pytorch_mf.py
@@ -7,8 +7,6 @@ num_epochs = 10000
 
 dfr = pd.read_csv('movie_data_unified.csv')
 dfr.columns = ['user_id', 'movie_id', 'Rating']
-rating_matrix = df.pivot(index='user_id', columns='movie_id', values=Rating)
-n_users, n_movies = rating_matrix.shape
 
 print((n_users, n_movies))
 
@@ -21,35 +19,33 @@ class MatrixFactorization(torch.nn.Module):
     def forward(self, user, item):
         return (self.user_factors(user) * self.item_factors(item)).sum(1)
 
-rating_matrix[rating_matrix.isna()] = 0
-rating_matrix = torch.from_numpy(rating_matrix.values)
-
 model = MatrixFactorization(n_users, n_movies)
 loss_func = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-6)
 
 users, movies = rating_matrix.nonzero()
 
+def iter(row, col, rating):
+    # Set gradients to zero
+    optimizer.zero_grad()
+
+    # Turn data into tensors
+    rating = torch.FloatTensor([rating])
+    row = torch.LongTensor([row])
+    col = torch.LongTensor([col])
+
+    # Predict and calculate loss
+    prediction = model(row, col)
+    loss = loss_func(prediction, rating)
+
+    # Backpropagate
+    loss.backward()
+
+    # Update the parameters
+    optimizer.step()
+
 for epoch in range(num_epochs):
     start = time.perf_counter()
-    p = np.random.permutation(25)
-    rows, cols = users[p], movies[p]
-    for row, col in zip(*(rows, cols)):
-        # Set gradients to zero
-        optimizer.zero_grad()
-
-        # Turn data into tensors
-        rating = torch.FloatTensor([rating_matrix[row, col]])
-        row = torch.LongTensor([row])
-        col = torch.LongTensor([col])
-
-        # Predict and calculate loss
-        prediction = model(row, col)
-        loss = loss_func(prediction, rating)
-
-        # Backpropagate
-        loss.backward()
-
-        # Update the parameters
-        optimizer.step()
+    batch = dfr.sample(25)
+    [iter(row[0], row[1], row[2]) for row in batch[['user', 'movie', 'rating']].to_numpy()]
     print(f'iteration {epoch} time: {time.perf_counter() - start}')
